590724ad5b0f017ceee22de2e9b8d447d8abd1e0
jhulyanne <jhulyannedeoliveira@gmail.com>
2024-11-03 15:37:23 -0300

    refactor: rebuild the code in a simpler way

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,26 @@ const formOrder = document.getElementById('newOrder');
 const formProduct = document.getElementById('newProduct');
 const tables = document.getElementById('tables');
 const btnEdit = document.getElementById('btnDel');
+const initialText = document.getElementById('initialText');
 let btnClicked = false;
 
 btnNewOrder.addEventListener('click', function(){
     btnClicked = !btnClicked;
 
     if(btnClicked) {
-        btnNewOrder.innerText = "Cancelar"
+        btnNewOrder.innerText = "Voltar"
         btnNewProduct.style.display = "none";
+        initialText.style.display = "none"
         formOrder.style.display = "block";
         formProduct.style.display = "none";
 
     } else {
         btnNewOrder.innerText = "Adicionar novo pedido"
         btnNewProduct.style.display = "block";
+        initialText.style.display = "block"
         formOrder.style.display = "none";
 
+        tables.style.display = "block";
     }
 })
 
@@ -27,7 +31,8 @@ btnNewProduct.addEventListener('click', function(){
     btnClicked = !btnClicked;
 
     if(btnClicked) {
-        btnNewProduct.innerText = "Cancelar";
+        btnNewProduct.innerText = "Voltar";
+        initialText.style.display = "none"
         btnNewOrder.style.display = "none";
         formOrder.style.display = "none";
         formProduct.style.display = "block";
@@ -35,6 +40,7 @@ btnNewProduct.addEventListener('click', function(){
         tables.style.display = "none";
     } else {
         btnNewProduct.innerText = "Registrar novo produto";
+        initialText.style.display = "block"        
         btnNewOrder.style.display = "block";
         formProduct.style.display = "none";
 
